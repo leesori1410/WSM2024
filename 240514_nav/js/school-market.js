afe904bb7055fecf880c1af06594867029bdb597
leesori1410 <w2319@e-mirim.hs.kr.com>
2024-06-18 15:39:10 +0900

    js 이름 검색 성공🎉

--- 240514_nav/js/school-market.js
+++ 240514_nav/js/school-market.js
@@ -1,4 +1,6 @@
 
+let allData;
+
 const showData = ((data) => {
     // data 하나씩 뽑아서 <article> -> .product-container의 자식으로 넣자 <-HTML
     const productContainerSection = document.getElementsByClassName("product-container")[0];
@@ -8,13 +10,14 @@ const showData = ((data) => {
         articleString += `<article class="product-item">
                 <img src="images/${element.image}" alt="${element.name}" class="product-image">
                 <div class="product-name">${element.name}</div>
-            </article>\n;`
+            </article>\n`
     });
     productContainerSection.innerHTML = articleString;
 
 });
 
 const setData = ((data) => {
+    allData = data;
     showData(data);
 });
 
@@ -26,3 +29,10 @@ const getData = (() => {
     .catch((error) => console.log(error));
 });
 getData();
+
+const searchData = (query) => {
+    // console.log(query);
+    // data 하나씩 꺼내어, name이랑 query 비교해서 있으며, 모아놓자
+    let searchData = allData.filter((oneData) => oneData["name"].includes(query));
+    showData(searchData);
+}
