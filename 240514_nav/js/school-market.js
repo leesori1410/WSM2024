b0033f785b0de099c0f33e596bbb293f0b8273fa
leesori1410 <w2319@e-mirim.hs.kr.com>
2024-06-18 15:11:22 +0900

    js data.json -> setData() -> showData()

--- 240514_nav/js/school-market.js
+++ 240514_nav/js/school-market.js
@@ -1,9 +1,28 @@
+
+const showData = ((data) => {
+    // data 하나씩 뽑아서 <article> -> .product-container의 자식으로 넣자 <-HTML
+    const productContainerSection = document.getElementsByClassName("product-container")[0];
+    
+    articleString = "";
+    data.forEach(element => {
+        articleString += `<article class="product-item">
+                <img src="images/${element.image}" alt="${element.name}" class="product-image">
+                <div class="product-name">${element.name}</div>
+            </article>\n;`
+    });
+    productContainerSection.innerHTML = articleString;
+
+});
+
+const setData = ((data) => {
+    showData(data);
+});
+
 const getData = (() => {
     const url = 'js/data.json';
     fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => setData(data))
     .catch((error) => console.log(error));
-
 });
 getData();
