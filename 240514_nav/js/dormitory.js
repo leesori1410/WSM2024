0c2fa10db509b926e1a670adb1cf14844bb438ac
leesori1410 <w2319@e-mirim.hs.kr.com>
2024-10-29 16:02:53 +0900

    dormitory 서버에서 데이터 받아서 js 변수에 넣는 연기

--- 240514_nav/js/dormitory.js
+++ 240514_nav/js/dormitory.js
@@ -1,3 +1,6 @@
+let allData;            // 초기 설정에 필요한 모든 데이터 : 세탁기, 시간, 호실
+let weeklyReservation;  // 미리 정해진 요일별 예약
+
 // selection-item 요소들 가져오자
 const selectionItemDivs = document.getElementsByClassName('selection-item');
 
@@ -8,6 +11,29 @@ const selectionRoomNameDiv = document.getElementById("selection-room-name");
 const boardDiv = document.getElementById("board");
 
 const pageDivs = [calendarDiv, selectionRoomNameDiv, selectionWashingmachineTimeDiv, boardDiv];
+
+// 초기 데이터 가져오자. allData.json, weekly-reservation.json 
+const initData = () => {
+    const getAllData = () => {
+        const url = 'js/allData.json';
+        fetch(url)
+        .then(response => response.json())
+        .then(data => allData = data)
+        .catch(error => console.log(error.message));
+    }
+    const getWeeklyReservation = async () => {
+        const url = 'js/weekly-reservation.json';
+        try{
+            const response = await fetch(url);
+            const data = await response.json();
+            weeklyReservation = data;
+        } catch(error){
+            console.log(error.message);
+        }
+    }
+    getAllData();
+    getWeeklyReservation();
+}
 
 const setPage = (page) => {
     //clear selection
@@ -28,4 +54,6 @@ const setPage = (page) => {
     // show pageDiv
     pageDivs[page-1].style.display = 'block';
 }
+
+initData();
 setPage(1);
