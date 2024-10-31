2fc457929cc7eae82eb83803af9a5055a152e88e
leesori1410 <w2319@e-mirim.hs.kr.com>
2024-10-31 10:18:42 +0900

    dormitory 1페이지: 날짜를 예약 정보에 넣고, 2페이지로 이동

--- 240514_nav/js/dormitory.js
+++ 240514_nav/js/dormitory.js
@@ -1,5 +1,7 @@
 let allData;            // 초기 설정에 필요한 모든 데이터 : 세탁기, 시간, 호실
 let weeklyReservation;  // 미리 정해진 요일별 예약
+let reservation;        // 사용자가 새롭게 지금 입력하는 예약 정보. 1페이지에서 초기화하자
+let reservations        // 사용자가 예약한 정보들의 덩어리
 
 // selection-item 요소들 가져오자
 const selectionItemDivs = document.getElementsByClassName('selection-item');
@@ -10,7 +12,7 @@ const selectionWashingmachineTimeDiv = document.getElementById('selection-washin
 const selectionRoomNameDiv = document.getElementById("selection-room-name");
 const boardDiv = document.getElementById("board");
 
-const pageDivs = [calendarDiv, selectionRoomNameDiv, selectionWashingmachineTimeDiv, boardDiv];
+const pageDivs = [calendarDiv, selectionWashingmachineTimeDiv, selectionRoomNameDiv, boardDiv];
 
 // 초기 데이터 가져오자. allData.json, weekly-reservation.json 
 const initData = () => {
@@ -53,6 +55,26 @@ const setPage = (page) => {
 
     // show pageDiv
     pageDivs[page-1].style.display = 'block';
+}
+
+const clickDate = (event) => {
+    // 예약 정보 초기화하자 
+    newReservation = {
+        "name" : undefined,
+        "room" : undefined,
+        "date" : undefined,
+        "time" : undefined,
+        "washingmachine" : undefined,
+        "notification" : true
+    }
+    // 날짜 data 가져오자
+    const dateString = event.target.dataset.date;
+    console.log(dateString);
+    const dateDate = new Date(dateString);
+    // 날짜 data 보관하자 
+    newReservation.date = dateDate;
+    // 2페이지로 가자
+    setPage(2);
 }
 
 initData();
